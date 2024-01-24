a6e91d98fe30ec78e055a9d385fab336b90fd32e
kangdaehoon <eogns47@konkuk.ac.kr>
2024-01-24 15:49:37 +0900

    ✨feat: add new features 1. can find IP of target domain 2. can find IPs of nameservers of domain 3. can find country code of IPs by Geoip

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/DNSSpy/zone-nameservers
+
+go 1.21.5
+
+require (
+	github.com/miekg/dns v1.1.58 // indirect
+	github.com/oschwald/geoip2-golang v1.9.0 // indirect
+	github.com/oschwald/maxminddb-golang v1.12.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net"
 	"os"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/miekg/dns"
+	"github.com/oschwald/geoip2-golang"
 )
 
 const (
@@ -22,12 +25,33 @@ const (
 
 func init() {
 	rand.Seed(time.Now().Unix())
+	// rand.NewSource(time.Now().Unix())
 }
 
 type ZoneNsResolver struct {
 	localm *dns.Msg
 	localc *dns.Client
 }
+
+func removeHTTPPrefix(url string) string {
+	ul := len(url)
+	if(url[ul-2]=='/'){
+		url = url[:ul-2]
+	}
+
+	// "http://" 또는 "https://"로 시작하는지 확인
+	if strings.HasPrefix(url, "http://") {
+		// "http://"이 있으면 잘라서 반환
+		return strings.TrimPrefix(url, "http://")
+	} else if strings.HasPrefix(url, "https://") {
+		// "https://"이 있으면 잘라서 반환
+		return strings.TrimPrefix(url, "https://")
+	}
+
+	// 위의 두 경우에 해당하지 않으면 원래의 URL 반환
+	return url
+}
+
 
 func NewZoneNsResolver() *ZoneNsResolver {
 	return &ZoneNsResolver{
@@ -43,10 +67,11 @@ func NewZoneNsResolver() *ZoneNsResolver {
 	}
 }
 
+//packet limit = 512 bytes
 func (zr *ZoneNsResolver) localQuery(qname string, qtype uint16, server string) (*dns.Msg, error) {
 	zr.localm.SetQuestion(qname, qtype)
 
-	r, _, err := zr.localc.Exchange(zr.localm, server+":53")
+	r, _, err := zr.localc.Exchange(zr.localm, server+":53")  
 	if err != nil {
 		return nil, err
 	}
@@ -67,13 +92,16 @@ func (zr *ZoneNsResolver) Resolve(zone string, server string) ([]string, error) 
 
 	var nameservers []string
 
+	
 	for _, ans := range r.Answer {
-		if t, ok := ans.(*dns.NS); ok {
+		if t, ok := ans.(*dns.NS); ok { //if(ans.*dns.NS is false, then ok is false)
 			nameserver := t.Ns
 			nameservers = append(nameservers, nameserver)
 		}
 	}
-
+	
+	
+	
 	if len(nameservers) == 0 {
 		// No "Answer" given by the server, check the Authority section if
 		// additional nameservers were provided.
@@ -84,7 +112,7 @@ func (zr *ZoneNsResolver) Resolve(zone string, server string) ([]string, error) 
 			}
 		}
 	}
-
+	
 	if len(nameservers) == 0 {
 		return nil, errors.New("No nameservers found for " + zone)
 	}
@@ -103,19 +131,54 @@ func domainToZones(domain string) []string {
 		assembled = pieces[i] + "." + assembled
 		zones = append(zones, assembled)
 	}
+
+
 	return zones
 }
+
+func getIPAddresses(url string) ([]string, error) {
+	// Using LookupHost to lookup IP addresses of domain
+	ips, err := net.LookupHost(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return ips, nil
+}
+
+func getCountryCode(ip string) (string, error) {
+	db, err := geoip2.Open("GeoLite2-Country.mmdb")
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		return "", fmt.Errorf("Invalid IP address: %s", ip)
+	}
+
+	record, err := db.Country(parsedIP)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.ToLower(record.Country.IsoCode), nil
+}
+
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("%s ZONE\n", os.Args[0])
 	}
 	domain := dns.Fqdn(os.Args[1])
+	domain = removeHTTPPrefix(domain)
 
 	conf, err := dns.ClientConfigFromFile("/etc/resolv.conf")
 	if err != nil || conf == nil {
 		log.Fatalf("Cannot initialize the local resolver: %s\n", err)
 	}
+
 
 	resolver := NewZoneNsResolver()
 
@@ -123,17 +186,21 @@ func main() {
 	nextNs := conf.Servers[0]
 
 	// split domain, query each part for NS records
-	for _, zone := range domainToZones(domain) {
-		if zone == "." {
-			fmt.Println("Retrieving list of root nameservers:")
-		} else {
-			fmt.Println("\nFinding nameservers for zone '" + zone + "' using parent nameserver '" + nextNs + "'")
+	for i, zone := range domainToZones(domain) {
+		if(i>2){
+			break
+		}
+
+		if zone != "." {
+			fmt.Println("🔎Finding nameservers for zone '" + zone + "' using parent nameserver '" + nextNs + "'\n")
 		}
 
 		ns, err = resolver.Resolve(zone, nextNs)
 		if err != nil {
-			log.Fatalln("Query failed: ", err)
+			log.Fatalln("🚨Query failed: ", err)
+		
 		}
+
 
 		// Pick a random NS record for the next queries
 		nextNs = ns[rand.Intn(len(ns))]
@@ -142,10 +209,54 @@ func main() {
 		for _, nameserver := range ns {
 			if nameserver == nextNs && domain != zone {
 				// We'll use this one for queries
-				fmt.Println(" ➡️ " + nameserver)
+				// fmt.Println(" ➡️ " + nameserver)
 			} else {
-				fmt.Println(" - " + nameserver)
+				// fmt.Println(" - " + nameserver)
 			}
 		}
+	}
+
+	fmt.Println("📜nameserver List:")
+	fmt.Println(ns)
+
+	var nameserverIPs []string
+
+	for _, nameserver := range ns {
+		IPs, err := getIPAddresses(nameserver)
+		if err != nil {
+        // 오류 처리
+			fmt.Println("Error:", err)
+			continue
+		}
+		nameserverIPs = append(nameserverIPs, IPs...)
+    }
+
+
+	// getIPAddresses 함수를 사용하여 URL에 대한 IP 주소 조회
+	ipAddresses, err := getIPAddresses(domain)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("\n📜IP Addresses for nameservers:")
+	for _, ip := range nameserverIPs {
+		countryCode, err := getCountryCode(ip)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(ip, countryCode)
+	}
+
+	// 조회된 IP 주소 출력
+	fmt.Println("\n📜IP Addresses for", domain, ":")
+	for _, ip := range ipAddresses {
+		countryCode, err := getCountryCode(ip)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(ip, countryCode)
 	}
 }
